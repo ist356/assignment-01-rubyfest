c39abb95fc50da57ac5ce4dd28695d021236e72f
rubyfest <srrubens@syr.edu>
2024-09-09 14:13:47 -0400

    finished the gpa and numbers assignments

--- code/numbers.py
+++ code/numbers.py
@@ -12,3 +12,15 @@ The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
 
+answer=1.1
+dictionary={'odd':[], 'even':[]}
+while answer!=0:
+    answer=int(input('Give an integer: '))
+    while round(answer)!=answer:
+        answer=int(input("Give an integer: "))
+    if answer!=0:
+        if answer%2==0:
+            dictionary['even'].append(answer)
+        elif answer%2!=0:
+            dictionary['odd'].append(answer)
+print(dictionary)
